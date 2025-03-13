7de099b92befbb288dc0d4de34f6f83eeaad8404
Probar Shamim <probarshamim@gmail.com>
2025-03-13 09:19:38 +0600

    Learning Data Type

---
+++ Java/DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+  public static void main(String[] args) {
+    boolean b = true;
+    char c = 'S';
+    short s = 12;
+    int i = 123456;
+    float f = 123.4321F;
+    double d = 445.56D;
+
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    System.out.println("s = " + s);
+    System.out.println("i = " + i);
+    System.out.println("f = " + f);
+    System.out.println("d = " + d);
+  }
+}
