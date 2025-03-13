8c20efed9e7d7eb6f40798d4f2cc6c7fc9e01883
Probar Shamim <probarshamim@gmail.com>
2025-03-13 08:33:45 +0600

    Assignment Product.java

---
+++ Java/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+  public static void main(String[] args) {
+    System.out.println("ID : 101");
+    System.out.println("Name : Iphone 15");
+    System.out.println("Name : Iphone 15");
+    System.out.println("Price : 1895 euros");
+    System.out.println("Catagegori : Phone");
+  }
+}
